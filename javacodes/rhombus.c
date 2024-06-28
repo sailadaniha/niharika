c7d131d29cd585532f1e2eafe52be422cee026c9
sailadaniha <147700566+sailadaniha@users.noreply.github.com>
2024-06-28 10:31:29 +0530

    Create rhombus.c

---
+++ javacodes/rhombus.c
@@ -0,0 +1,30 @@
+public class RhombusPattern {
+    public static void main(String[] args) {
+        int rows = 10; 
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        for (int i = rows - 1; i >= 1; i--) {
+            
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
